a971637d5e633d0ee843c4b255cc80e403864af2
Rafiul Hasan <67283985+rafidoth@users.noreply.github.com>
2023-07-14 01:50:39 +0600

    init

---
+++ 121.js
@@ -0,0 +1,28 @@
+const prices = [2,1,2,1,0,1,2]
+var maxProfit = function(prices) {
+    let [maxprofit, buy] = [0,0]
+    for(let sell =1;sell<prices.length;sell++){
+        if(prices[buy]>prices[sell]){
+            buy=sell;
+        }
+        maxprofit = Math.max(maxprofit,prices[sell]-prices[buy])
+    }
+    return maxprofit
+};
+
+
+//time complexity O(n^2)  memory O(1)
+// var maxProfit = function(prices) {
+//     let max_profit = 0;
+//     for(let i =0;i<prices.length;i++){
+//         for(let j=i+1;j<prices.length;j++){
+//             const profit = prices[j] - prices[i];
+//             if(profit>max_profit) max_profit= profit;
+//         }
+//     }
+//     return max_profit
+// };
+
+
+
+console.log(maxProfit(prices))
